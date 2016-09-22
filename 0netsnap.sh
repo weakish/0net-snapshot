830fb35e20cee2a274ee3edf9f476bb04e437b2d
Jakukyo Friel <weakish@gmail.com>
2016-09-22 19:31:30 +0800

    0netsnap: fix bugs.

--- 0netsnap.sh
+++ 0netsnap.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-your_zeronet_folder="$HOME/ZeroBundle/ZeroNet/data"
+your_zeronet_folder="/pool/src/ZeroNet/data"
 
 cd $your_zeronet_folder
 # This assumes git operations (blocking) are fast.
 # It is better to use a queue, e.g.
 # https://github.com/leifdenby/shellqueue
 # Or switch to another language with decent async queue library.
-inotifywait -mrq -e create,delete,modify \
-    --exclude ".*\.db"  --exclude ".*\.db-journal"
+inotifywait -mrq -e delete,close_write \
+    --exclude ".*\.db"  --exclude ".*\.db-journal" --exclude "content.json"
     "$your_zeronet_folder" | while read path event name ; do
     if [ $event = 'DELETE' ]; then
         # Give meaning error message if git repository does not exist.
-        git rm "$path/name"
+        git rm "$path$name"
     else
-        git add "$path/name"
+        git add "$path$name"
     fi
     # TODO Use separated git repositories per site.
-    git commit -m 'Auto commit.' "$path/name"
+    echo git commit -m 'Auto commit.'
 done
